1689bc3b0c736bd12a46804561a5cdb301427e6e
Phương Xương Quang <quangpx1@fsoft.com.vn>
2016-06-23 18:07:27 +0700

    Init

--- SportChannel/ViewController.swift
+++ SportChannel/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    var webView: WKWebView!
+    var webServer:GCDWebServer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        webView = WKWebView(frame: self.view.frame)
+        
+        self.view.addSubview(webView)
+        let baseURL = NSBundle.mainBundle().bundleURL
+        print("URL : " + baseURL.absoluteString)
+        if #available(iOS 9.0, *) {
+            initWebServer(NSURL.fileURLWithPath("sportchannel_web", relativeToURL: baseURL).absoluteURL)
+        } else {
+            // Fallback on earlier versions
+            let urlStr = NSString.init(format: "%@/%@", baseURL, "sportchannel_web") as String
+            initWebServer(NSURL(fileURLWithPath: urlStr))
+        }
+        
+        let basePath = NSURL(string: "http://localhost:9090/index.html")
+        let request = NSURLRequest(URL: basePath!)
+        webView.loadRequest(request)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func initWebServer(url:NSURL) -> Void {
+        webServer = GCDWebServer()
+        
+        webServer.addDefaultHandlerForMethod("GET", requestClass: GCDWebServerRequest.classForCoder(), processBlock: {(request:GCDWebServerRequest?) -> GCDWebServerResponse in
+            let page = request!.URL
+            let path = page.path
+            let filePath = NSString(format: "%@%@",url , path!)
+            let sfilePath = filePath.substringFromIndex(7) as String
+            if (page.lastPathComponent!.hasSuffix("html")) {
+                var html = ""
+                do {
+                    try html = NSString(contentsOfFile: sfilePath, encoding: NSUTF8StringEncoding) as String
+                }
+                catch {
+                    html = ""
+                }
+                return GCDWebServerDataResponse(HTML: html)
+            } else {
+                let data = NSData(contentsOfFile: sfilePath)
+                if(data != nil) {
+                    var type = "image/jpeg"
+                    if(page.lastPathComponent!.hasSuffix("jpg")) {
+                        type = "image/jpeg"
+                    } else if(page.lastPathComponent!.hasSuffix("png")){
+                        type = "image/png"
+                    } else if(page.lastPathComponent!.hasSuffix("css")){
+                        type = "text/css"
+                    } else if(page.lastPathComponent!.hasSuffix("js")){
+                        type = "text/javascript"
+                    } else {
+                        return GCDWebServerDataResponse(data: data, contentType: "text")
+                    }
+                    return GCDWebServerDataResponse(data: data, contentType: type)
+                } else {
+                    return GCDWebServerDataResponse(HTML: NSString(format: "<html><body><p>404 : unknown file %@ World</p></body></html>",page.absoluteString) as String)
+                }
+            }
+        })
+        webServer.startWithPort(9090, bonjourName: "")
+        print("Server start with URL : " + webServer.serverURL.absoluteString)
+    }
 }
 
---
+++ SportChannel/WebServer-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  WebServer-Bridging-Header.h
+//  SportChannel
+//
+//  Created by Quang Phương on 6/23/16.
+//  Copyright © 2016 Quang Phương. All rights reserved.
+//
+
+#ifndef WebServer_Bridging_Header_h
+#define WebServer_Bridging_Header_h
+
+#import "GCDWebServer.h"
+#import "GCDWebServerDataResponse.h"
+
+#endif /* WebServer_Bridging_Header_h */
